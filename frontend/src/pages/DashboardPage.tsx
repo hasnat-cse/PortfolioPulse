120e58d28b69b0461c0a78609aecfac678deb0e4
Arif Hasnat <hasnat.cse.buet@gmail.com>
2026-09-15 01:06:07 -0600

    Add dashboard page

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,10 @@
+function DashboardPage() {
+  return (
+    <main>
+      <h2>Dashboard</h2>
+      <p>Your Portfolio overview will appear here.</p>
+    </main>
+  );
+}
+
+export default DashboardPage;
